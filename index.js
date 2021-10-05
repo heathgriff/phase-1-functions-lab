584c8facb7c54dca746cf6c037bf6fc8a1ac0cd9
Heath Griffith <heathlgriffith@gmail.com>
2021-10-05 10:28:33 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(streetNumber) {
+    return Math.abs(streetNumber - 42);
+}
+
+function distanceFromHqInFeet(streetNumber) {
+    return distanceFromHqInBlocks(streetNumber) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    return Math.abs(start - destination) * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination) - 400;
+    if (distanceInFeet <= 0) {
+        return 0;
+    } else if (distanceInFeet <= 1600) {
+        return distanceInFeet * .02;
+    } else if (distanceInFeet <= 2100) {
+        return 25;
+    } else if (distanceInFeet > 2100) {
+        return 'cannot travel that far';
+    }
+}
